9d42e0c237b094663c0ab5233e5af730f255743f
19skb95 <19skb95@gmail.com>
2020-11-09 10:37:54 +0530

    CB Commit:9-Nov-2020

---
+++ Dashborad_CB/BankDashboard/Common/ViewModelClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankDashboard.Common
+{
+    public class ViewModelClass
+    {
+        
+      
+        public class selectPages
+        {
+            public string PageName { get; set; }
+            public string groupname { get; set; }
+            public string groupdesc { get; set; }
+        }
+
+        //public class ViewUserDetail
+        //{
+        //    public string UserName { get; set; }
+        //    public string GroupName { get; set; }
+        //    public string GroupPages { get; set; }
+        //}
+    }
+}
---
+++ Dashborad_CB/BankDashboard/Controllers/CBController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BankDashboard.Controllers
+{
+    public class CBController : Controller
+    {
+        // GET: CB
+        public ActionResult Index()
+        {
+            ViewBag.botstat = "active";
+            return View();
+        }
+        public ActionResult WC()
+        {
+            ViewBag.wcstat = "active";
+            try
+            {
+
+            }
+            catch(Exception ex)
+            {
+                TempData["Error"] = "Somthing went wrong.."+ex.Message;
+            }
+            return View();
+        }
+    }
+}
